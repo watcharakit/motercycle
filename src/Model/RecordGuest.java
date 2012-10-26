c2796eb28af9e61df513868beb449196709966b3
watcharakit <gun_1288@hotmail.com>
2012-10-27 02:33:10 +0700

    recordhistorycar,recordguest

---
+++ src/Model/RecordGuest.java
@@ -0,0 +1,78 @@
+package Model;
+
+public class RecordGuest {
+	String nameBuyer;
+	int cardnumBuyer;
+	String addressBirthBuyer;
+	String addressPresentBuyer;
+	String careerBuyer;
+
+	String nameBondsman;
+	int cardnumBondsman;
+	String addressBirthBondsman;
+	String addressPresentBondsman;
+	String careerBondsman;
+	public String getNameBuyer() {
+		return nameBuyer;
+	}
+	public void setNameBuyer(String nameBuyer) {
+		this.nameBuyer = nameBuyer;
+	}
+	public int getCardnumBuyer() {
+		return cardnumBuyer;
+	}
+	public void setCardnumBuyer(int cardnumBuyer) {
+		this.cardnumBuyer = cardnumBuyer;
+	}
+	public String getAddressBirthBuyer() {
+		return addressBirthBuyer;
+	}
+	public void setAddressBirthBuyer(String addressBirthBuyer) {
+		this.addressBirthBuyer = addressBirthBuyer;
+	}
+	public String getAddressPresentBuyer() {
+		return addressPresentBuyer;
+	}
+	public void setAddressPresentBuyer(String addressPresentBuyer) {
+		this.addressPresentBuyer = addressPresentBuyer;
+	}
+	public String getCareerBuyer() {
+		return careerBuyer;
+	}
+	public void setCareerBuyer(String careerBuyer) {
+		this.careerBuyer = careerBuyer;
+	}
+	public String getNameBondsman() {
+		return nameBondsman;
+	}
+	public void setNameBondsman(String nameBondsman) {
+		this.nameBondsman = nameBondsman;
+	}
+	public int getCardnumBondsman() {
+		return cardnumBondsman;
+	}
+	public void setCardnumBondsman(int cardnumBondsman) {
+		this.cardnumBondsman = cardnumBondsman;
+	}
+	public String getAddressBirthBondsman() {
+		return addressBirthBondsman;
+	}
+	public void setAddressBirthBondsman(String addressBirthBondsman) {
+		this.addressBirthBondsman = addressBirthBondsman;
+	}
+	public String getAddressPresentBondsman() {
+		return addressPresentBondsman;
+	}
+	public void setAddressPresentBondsman(String addressPresentBondsman) {
+		this.addressPresentBondsman = addressPresentBondsman;
+	}
+	public String getCareerBondsman() {
+		return careerBondsman;
+	}
+	public void setCareerBondsman(String careerBondsman) {
+		this.careerBondsman = careerBondsman;
+	}
+	
+	
+
+}
---
+++ src/Model/RecordHistoryCar.java
@@ -0,0 +1,248 @@
+package Model;
+
+public class RecordHistoryCar {
+	
+	int PatentDay;
+	String CarNum;
+	String County;
+	String TypeCar;
+	String StyleCar;
+	String BrandCar;
+	String MotorNum;
+	String SeatMotorNum;
+	String Fuel;
+	String GasTankNum;
+	int PlungerAmount;
+	int CCNum;
+	String HorseProwKW;
+	String Notes;
+	Double WeightCar;
+	Double load;
+	Double WeightInclud;
+	int Seating;
+	
+	int Oder;
+	int PossessDay;
+	String Occupant1;
+	int CardNum1;
+	int BrithDay1;
+	String Netionality1;
+	String Address1;
+	double phone1;
+	String Occupant2;
+	int CardNum2;
+	int BrithDay2;
+	String Netionality2;
+	String Address2;
+	double phone2;
+	String contractNum;
+	String contractDay;
+	
+	
+	public int getPatentDay() {
+		return PatentDay;
+	}
+	public void setPatentDay(int patentDay) {
+		PatentDay = patentDay;
+	}
+	public String getCarNum() {
+		return CarNum;
+	}
+	public void setCarNum(String carNum) {
+		CarNum = carNum;
+	}
+	public String getCounty() {
+		return County;
+	}
+	public void setCounty(String county) {
+		County = county;
+	}
+	public String getTypeCar() {
+		return TypeCar;
+	}
+	public void setTypeCar(String typeCar) {
+		TypeCar = typeCar;
+	}
+	public String getStyleCar() {
+		return StyleCar;
+	}
+	public void setStyleCar(String styleCar) {
+		StyleCar = styleCar;
+	}
+	public String getBrandCar() {
+		return BrandCar;
+	}
+	public void setBrandCar(String brandCar) {
+		BrandCar = brandCar;
+	}
+	public String getMotorNum() {
+		return MotorNum;
+	}
+	public void setMotorNum(String motorNum) {
+		MotorNum = motorNum;
+	}
+	public String getSeatMotorNum() {
+		return SeatMotorNum;
+	}
+	public void setSeatMotorNum(String seatMotorNum) {
+		SeatMotorNum = seatMotorNum;
+	}
+	public String getFuel() {
+		return Fuel;
+	}
+	public void setFuel(String fuel) {
+		Fuel = fuel;
+	}
+	public String getGasTankNum() {
+		return GasTankNum;
+	}
+	public void setGasTankNum(String gasTankNum) {
+		GasTankNum = gasTankNum;
+	}
+	public int getPlungerAmount() {
+		return PlungerAmount;
+	}
+	public void setPlungerAmount(int plungerAmount) {
+		PlungerAmount = plungerAmount;
+	}
+	public int getCCNum() {
+		return CCNum;
+	}
+	public void setCCNum(int cCNum) {
+		CCNum = cCNum;
+	}
+	public String getHorseProwKW() {
+		return HorseProwKW;
+	}
+	public void setHorseProwKW(String horseProwKW) {
+		HorseProwKW = horseProwKW;
+	}
+	public String getNotes() {
+		return Notes;
+	}
+	public void setNotes(String notes) {
+		Notes = notes;
+	}
+	public Double getWeightCar() {
+		return WeightCar;
+	}
+	public void setWeightCar(Double weightCar) {
+		WeightCar = weightCar;
+	}
+	public Double getLoad() {
+		return load;
+	}
+	public void setLoad(Double load) {
+		this.load = load;
+	}
+	public Double getWeightInclud() {
+		return WeightInclud;
+	}
+	public void setWeightInclud(Double weightInclud) {
+		WeightInclud = weightInclud;
+	}
+	public int getSeating() {
+		return Seating;
+	}
+	public void setSeating(int seating) {
+		Seating = seating;
+	}
+	public int getOder() {
+		return Oder;
+	}
+	public void setOder(int oder) {
+		Oder = oder;
+	}
+	public int getPossessDay() {
+		return PossessDay;
+	}
+	public void setPossessDay(int possessDay) {
+		PossessDay = possessDay;
+	}
+	public String getOccupant1() {
+		return Occupant1;
+	}
+	public void setOccupant1(String occupant1) {
+		Occupant1 = occupant1;
+	}
+	public int getCardNum1() {
+		return CardNum1;
+	}
+	public void setCardNum1(int cardNum1) {
+		CardNum1 = cardNum1;
+	}
+	public int getBrithDay1() {
+		return BrithDay1;
+	}
+	public void setBrithDay1(int brithDay1) {
+		BrithDay1 = brithDay1;
+	}
+	public String getNetionality1() {
+		return Netionality1;
+	}
+	public void setNetionality1(String netionality1) {
+		Netionality1 = netionality1;
+	}
+	public String getAddress1() {
+		return Address1;
+	}
+	public void setAddress1(String address1) {
+		Address1 = address1;
+	}
+	public double getPhone1() {
+		return phone1;
+	}
+	public void setPhone1(double phone1) {
+		this.phone1 = phone1;
+	}
+	public String getOccupant2() {
+		return Occupant2;
+	}
+	public void setOccupant2(String occupant2) {
+		Occupant2 = occupant2;
+	}
+	public int getCardNum2() {
+		return CardNum2;
+	}
+	public void setCardNum2(int cardNum2) {
+		CardNum2 = cardNum2;
+	}
+	public int getBrithDay2() {
+		return BrithDay2;
+	}
+	public void setBrithDay2(int brithDay2) {
+		BrithDay2 = brithDay2;
+	}
+	public String getNetionality2() {
+		return Netionality2;
+	}
+	public void setNetionality2(String netionality2) {
+		Netionality2 = netionality2;
+	}
+	public String getAddress2() {
+		return Address2;
+	}
+	public void setAddress2(String address2) {
+		Address2 = address2;
+	}
+	public double getPhone2() {
+		return phone2;
+	}
+	public void setPhone2(double phone2) {
+		this.phone2 = phone2;
+	}
+	public String getContractNum() {
+		return contractNum;
+	}
+	public void setContractNum(String contractNum) {
+		this.contractNum = contractNum;
+	}
+	public String getContractDay() {
+		return contractDay;
+	}
+	public void setContractDay(String contractDay) {
+		this.contractDay = contractDay;
+	}
+	
+
+}
